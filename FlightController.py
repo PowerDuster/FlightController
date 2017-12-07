7c33f058a61cabcfbe9639e5d05a3da01297eb0a
Sparrow <herstalf@gmail.com>
2017-12-07 21:24:43 +0500

    misc.

--- FlightController.py
+++ FlightController.py
@@ -1,28 +1,42 @@
 import threading
 import socket
+import serial
 
-pitch=0, yaw=0, roll=0
-
+pitch=0
+yaw=0
+roll=0
+isConnected=False
+fly=False
+global addr
 
 def controlServerThread():
-	controlSock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(('', 3000))
-	global pitch, yaw, roll
-	while True:
-		msg, addr = controlSock.recvfrom()
-		
+    print('Control server running')
+    controlSock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    controlSock.bind(('', 3000))
+    global pitch, yaw, roll, isConnected, addr
+    while True:
+        msg, addr = controlSock.recvfrom(16)
+        print('Control signal received')
+        isConnected=True
+    
+    print('Control server exit')
+
 
 def flightServerThread():
-	sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(('', 3434))
-	while True:
-		data = sock.recv(24)
+    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', 3434))
+    while True:
+        data = sock.recv(24)
 
+
+ser=serial.Serial('', 9600, timeout=0.01)
 
 cThread=threading.Thread(target=controlServerThread)
 cThread.start()
-fThread=threading.Thread(target=flightServerThread)
-fThread.start()
 
 while True:
-	#get sensor data
+    sensorData=ser.readline
+    #parse sensorData
+    if isConnected:
+        print(addr)
+    
