71aba72a8a48dc3c469abbcee1f15b91856f67f3
Barksalot <herstalf@gmail.com>
2017-12-14 19:42:20 +0500

    transporting

--- FlightController.py
+++ FlightController.py
@@ -9,13 +9,13 @@ yaw=0
 roll=0
 thrust=0
 isConnected=False
-Run=False
+Run=True
 fly=False
 global addr
 
 def handler(sigtype, frame):
-	Run=False
-	os._exit(0)
+    Run=False
+    os._exit(0)
 
 def controlServerThread():
     print('Control server running')
@@ -23,34 +23,32 @@ def controlServerThread():
     controlSock.bind(('', 7777))
     global pitch, yaw, roll, addr, isConnected
     isConnected=True
-    while True:
-        msg, addr = controlSock.recvfrom(16)
-        controlSock.sendto(b'172.15.66.176', ('172.15.74.234', 7777))
-        if msg == "connect":
-            isConnected=True
-        print('Control signal received')
-    
-    print('Control server exit')
 
 
 def flightServerThread():
     sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', 6666))
+    global yaw, pitch
     while True:
-        data = sock.recv(14)
-        print(data)
-        #yaw, pitch = data.split(",")
+        #print('recv')
+        data = sock.recv(1024).decode("utf-8") 
+        #print('recv\'d')
+        #print(data)
+        yaw, pitch = data.split(",")
+        #print(yaw)
+        #print(pitch)
 
 #ser=serial.Serial('', 9600, timeout=0.01)
 
-def getSensorData():
-    #maybe use a fifo queue
-    l, r, f, b, d = ser.read(14).split("")# Or readline
-    sendData()
+#def getSensorData():
+#    #maybe use a fifo queue
+#    l, r, f, b, d = ser.read(14).split("")# Or readline
+#    sendData()
     
 
 def main():
     global isConnected
+    global yaw, pitch, roll
     signal.signal(signal.SIGINT, handler)
     cThread=threading.Thread(target=controlServerThread)
     cThread.start()
@@ -58,11 +56,11 @@ def main():
     fThread.start()
     
     while Run:
-        print('\nPitch:')
-        print(pitch)
-        print('Yaw:')
-        print(yaw)
-        time.sleep(1)
+        #print(yaw)
+        #print(pitch)
+        os.system('cls')
+        print(float(yaw)*10,'\n',float(pitch)*10,'\r'),
+        time.sleep(0.01)
         #while isConnected:
             #if f>40 and b>40:
                 #setLeftServo(pitch+roll)
