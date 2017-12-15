f84b3f79d24a20c5f1c660ba6d0a2f626d66728d
PowerDuster <herstalf@gmail.com>
2017-12-16 00:54:13 +0500

    major

--- FlightController.py
+++ FlightController.py
@@ -3,93 +3,85 @@ import socket
 import signal
 import os
 import time
+import serial
+import RPi.GPIO as GPIO
+from random import *
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(3, GPIO.OUT)
+##GPIO.setup(5, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
+##GPIO.setup(11, GPIO.OUT)
+fr = GPIO.PWM(3, 50)
+##fl = GPIO.PWM(5, 50)
+sr = GPIO.PWM(7, 50)
+##sl = GPIO.PWM(11, 50)
+fr.start(7.5)
+##fl.start(7.5)
+sr.start(7.5)
+##sl.start(7.5)
 
 pitch=0
-yaw=0
 roll=0
 thrust=0
-isConnected=False
-Run=True
-fly=False
-global addr
-
-def handler(sigtype, frame):
-    Run=False
-    os._exit(0)
-
-def controlServerThread():
-    print('Control server running')
-    controlSock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    controlSock.bind(('', 7777))
-    global pitch, yaw, roll, addr, isConnected
-    isConnected=True
-
+##serl=serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)
 
 def flightServerThread():
+    global roll, pitch, sock
     sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', 6666))
-    global yaw, pitch
     while True:
-        #print('recv')
-        data = sock.recv(1024).decode("utf-8") 
-        #print('recv\'d')
-        #print(data)
-        yaw, pitch = data.split(",")
-        #print(yaw)
-        #print(pitch)
+        data = sock.recv(1024)
+        roll, pitch = data.split(",")
+        pitch = round(float(pitch), 2) + 90
+        roll = round(float(roll), 2) + 90
 
-#ser=serial.Serial('', 9600, timeout=0.01)
+##def SensorDataThread():
+##    global alt, forw, back, right
+##    while True:
+##        sensorData = serl.readline()
+##        print(sensorData)
+##        alt, forw, back, right = (serl.readline()).split(",")
+##        print(alt)
+##        print(forw)
+##        print(back)
+##        print(right)
 
-#def getSensorData():
-#    #maybe use a fifo queue
-#    l, r, f, b, d = ser.read(14).split("")# Or readline
-#    sendData()
-    
+
+def handler(sigtype, frame):
+    global sock
+    sock.close()
+    fr.stop()
+    sr.stop()
+    GPIO.cleanup()
+    os._exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 def main():
-    global isConnected
-    global yaw, pitch, roll
-    signal.signal(signal.SIGINT, handler)
-    cThread=threading.Thread(target=controlServerThread)
-    cThread.start()
     fThread=threading.Thread(target=flightServerThread)
     fThread.start()
+##    sThread=threading.Thread(target=SensorDataThread)
+##    sThread.start()
     
-    while Run:
-        #print(yaw)
-        #print(pitch)
-        os.system('cls')
-        print(float(yaw)*10,'\n',float(pitch)*10,'\r'),
-        time.sleep(0.01)
-        #while isConnected:
-            #if f>40 and b>40:
-                #setLeftServo(pitch+roll)
-                #setRightServo(pitch-roll)
-                ##increase rpm to compensate
-            #elif f<40:
-                #setLeftServo(-35)
-                #setRightServo(-35)
-                ##increase rpm to compensate
-            #elif b<40:
-                #setLeftServo(35)
-                #setRightServo(35)
-                ##increase rpm to compensate
-            #if l>40 and r>40:
-                #setSidewayLeftServo(yaw)
-                #setSidewayRightServo(yaw)
-                ##increase rpm to compensate
-            #elif l<40:
-                #setSidewayLeftServo(35)
-                #setSidewayRightServo(35)
-                ##increase rpm to compensate
-            #elif r<40:
-                #setSidewayLeftServo(-35)
-                #setSidewayRightServo(-35)
-                ##increase rpm to compensate
-            #if d>40:
-                #setLeftSpeed(thrust)
-                #setRightSpeed(thrust)
-                ##increase rpm to compensate
+    while True:
+##        os.system('clear')
+##        print(pitch)
+##        print(roll)
+        pD = float(pitch)/12
+        if pD>24/12 and pD<100:
+            fr.ChangeDutyCycle(pD)
+        rD = float(roll)/12
+        if rD>24/12 and rD<100:
+            sr.ChangeDutyCycle(rD)
+##        x = round(pitch/12, 2)
+##        if x>0 and x<100:
+##            fr.ChangeDutyCycle(x)
+##        print(x)
+##        print(x)
+##        print('')
+##        time.sleep(3)
 
 
 if __name__=='__main__':
